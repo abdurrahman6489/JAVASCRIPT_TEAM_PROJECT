6e9000ad6d2d1c4ba71267c59fb93a764c045f2a
anandrajaaryan <anandrajaaryan@gmail.com>
2023-03-31 22:13:23 +0530

    adding video

--- ANANDRAJAARYAN/script.js
+++ ANANDRAJAARYAN/script.js
@@ -4,8 +4,8 @@ const foodSound = new Audio('./music/food.mp3');
 const gameOverSound = new Audio('./music/gameover.mp3');
 const moveSound = new Audio('./music/move.mp3');
 const musicSound = new Audio('./music/music.mp3');
-let speed = 0.5;
-let speedInc = 0.05;
+let speed = 15;
+// let speedInc = 0.05;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -18,7 +18,7 @@ food = {x: 6, y: 7};
 function main(ctime) {
     window.requestAnimationFrame(main);
     // console.log(ctime) 
-    speed += speedInc;
+    // speed += speedInc;
     if((ctime - lastPaintTime)/1000 < 1/speed){
         return;
     }
